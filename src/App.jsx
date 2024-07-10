e2208cb43725fe76b2c2271357d02622482da772
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-07-10 12:00:35 -0700

    Added follow mouse feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,61 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
-import './App.css'
+// import { useState } from 'react'
+// import { useEffect } from 'react'
+// import './App.css'
 
-function App() {
-const [counter, setCounter] = useState(0);
-const [state, setState] = useState(0);
+// function App() {
+// const [counter, setCounter] = useState(0);
+// const [state, setState] = useState(0);
 
-// useEffect
-  useEffect(()=> {
-      //All the logic you want to run with your useEffect
-      let rand =Math.random();
-      console.log(rand);
-    console.log('I have loaded');
-  }, [counter, state])
+// // useEffect
+//   useEffect(()=> {
+//       //All the logic you want to run with your useEffect
+//       let rand =Math.random();
+//       console.log(rand);
+//     console.log('I have loaded');
+//   }, [counter, state])
+//   return (
+//     <>
+//       <h1> My Application</h1>
+//       <button onClick={()=>{setCounter(x=> x + 1)}}>Click Me</button>
+//       <h1>{counter}</h1>
+//     </>
+//   )
+// }
+
+// export default App
+
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [position, setPosition] = useState({ x: 50, y: 50 });
+  const [canMove, setCanMove] = useState(true);
+
+  useEffect(() => {
+    function handleMove(e) {
+      setPosition({ x: e.clientX, y: e.clientY})}
+      if (canMove) {
+        window.addEventListener('pointermove', handleMove);
+      }
+      return () => { window.removeEventListener('pointermove', handleMove)};
+    }, [canMove]);
+
   return (
-    <>
-      <h1> My Application</h1>
-      <button onClick={()=>{setCounter(x=> x + 1)}}>Click Me</button>
-      <h1>{counter}</h1>
-    </>
-  )
+    <div onClick={() => setCanMove(!canMove)} className="container">
+      <h1>Click to {canMove ? "Stop Following" : "Follow"} Cursor</h1>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "blue",
+          borderRadius: "50%",
+          opacity: 0.6,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+          pointerEvents: "none",
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40
+        }}
+      />
+    </div>
+  );
 }
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </StrictMode>
+);
