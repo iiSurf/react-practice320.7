eee45ac03dff39f101dc81873264581e929aca4c
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-07-10 10:31:55 -0700

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [counter, setCounter] = useState(0);
+const [state, setState] = useState(0);
 
+// useEffect
+  useEffect(()=> {
+      //All the logic you want to run with your useEffect
+      let rand =Math.random();
+      console.log(rand);
+    console.log('I have loaded');
+  }, [counter, state])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1> My Application</h1>
+      <button onClick={()=>{setCounter(x=> x + 1)}}>Click Me</button>
+      <h1>{counter}</h1>
     </>
   )
 }
